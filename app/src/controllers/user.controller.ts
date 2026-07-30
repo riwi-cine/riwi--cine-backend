e7afb1eaf7d69abec4b046a8bcb2691b17eee3dd
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-07-30 07:05:15 -0500

    login

--- app/src/controllers/user.controller.ts
+++ app/src/controllers/user.controller.ts
@@ -159,23 +159,23 @@ export const getUsers = async (_req: Request, res: Response): Promise<Response> 
     }
 
 };
-
-export const findUser = async (_req: Request, res: Response): Promise<Response> => {
-
+export const findUser = async (req: Request, res: Response): Promise<Response> => {
     try {
+        const { email, password } = req.body;
 
-        // Solicita la información al servicio.
-        const user = await userService.findOne();
+        if (!email || !password) {
+            return res.status(400).json({
+                error: "El email y la contraseña son obligatorios."
+            });
+        }
 
-        // Retorna la colección de usuarios.
+        const user = await userService.findOne(email, password);
+
         return res.status(200).json(user);
 
     } catch (error: any) {
-
-        return res.status(500).json({
+        return res.status(401).json({
             error: error.message
         });
-
     }
-
 };

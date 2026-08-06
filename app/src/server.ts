0ef19d80255854f0fae8b98b0f625457756320d3
EfrainMartinez1672 <hlaluz59@gmail.com>
2026-08-06 12:43:47 -0500

    fix: add depends

--- app/src/server.ts
+++ app/src/server.ts
@@ -12,10 +12,12 @@ import { swaggerSpec } from "./docs/swagger";
 
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Rutas
 app.use("/api/users", userRoutes);
--- app/src/utils/jwt.ts
+++ app/src/utils/jwt.ts
@@ -1,13 +1,17 @@
 import jwt, {SignOptions} from 'jsonwebtoken'
 import { UserPayload } from '../services/interfaces/user.payload.interface'
 
+ /**
+  * funciones principales del jwt
+  */
+
 const JWT_SECRET = process.env.JWT_SECRET
 
 if (!JWT_SECRET) {
     throw new Error('JWT_SECRET no está definido en las variables de entorno');
 };
 
-export const GenerateToken = (payload: Omit<UserPayload, 'iat' | 'exp'>):string => {
+export const generateToken = (payload: Omit<UserPayload, 'iat' | 'exp'>):string => {
     const options: SignOptions = {
         expiresIn:(process.env.JWT_EXPIRES_IN as SignOptions['expiresIn'])
     }

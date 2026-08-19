e60bbe4ef7309864dd957dfa7195960c3fd0a5e3
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-08-19 07:15:37 -0500

    feat: swagger name updated

--- app/src/docs/swagger.ts
+++ app/src/docs/swagger.ts
@@ -32,7 +32,7 @@ const options: swaggerJSDoc.Options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "API RIWI Cinema",
+            title: "MineRoyal",
             version: "1.0.0",
             description: "Swagger api endpoints",
         },
